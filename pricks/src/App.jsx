a61757df2f912303d27e4e352c0624610b4a3375
Lorenz Hohermuth <lofloho@gmail.com>
2023-03-16 18:26:30 +0100

    bug fixes

--- pricks/src/App.jsx
+++ pricks/src/App.jsx
@@ -4,93 +4,52 @@ import SetColors from './SetColors';
 
 function App() {
 
-  function setValue(e) {
-    e.target.value.charAt(0) == '#' ? 
-    setBgColor(e.target.value)
-    : 
-    setBgColor('#' + e.target.value)
+  const [bgColor, setBgColor] = useState('#F6F1F1');
+  const [prColor, setPrColor] = useState('#19A7CE');
+  const [secColor, setSecColor] = useState('#146C94');
+  const [textColor, setTextColor] = useState('#000000');
+  
+  useEffect(() => {
+    SetColors([bgColor, secColor, prColor, textColor])
+  }, [bgColor, secColor, prColor, textColor])
 
+  function setValue(e, func) {
+    const x = e.target.value
+    x.charAt(0) == '#' && x.length == 7 || x.length == 4?
+      func(x)
+    : 
+    x.charAt(0) != '#' && x.length == 6 || x.length == 3?
+    func('#' + e.target.value)
+    :
+    console.log('no hex falue');
   }
 
   function rgbToHex(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
   
-  const [bgColor, setBgColor] = useState('#F6F1F1');
-  const [prColor, setPrColor] = useState('#19A7CE');
-  const [secColor, setSecColor] = useState('#146C94');
-  const [textColor, setTextColor] = useState('#000000');
-
-  useEffect(() => {
-    SetColors([bgColor, secColor, prColor, textColor])
-  }, [bgColor, secColor, prColor, textColor])
-
-  function setBg(e) {
-    x = e.target.value
-    x.charAt(0) == '#' && x.length == 7 ? 
-      setBgColor(x)
-    : 
-    x.charAt(0) != '#' && x.length == 6 ?
-    setBgColor('#' + e.target.value)
-    :
-    console('no hex falue');
-  }
-
-  function setPr(e) {
-    x = e.target.value
-    x.charAt(0) == '#' && x.length == 7 ? 
-      setPrColor(x)
-    : 
-    x.charAt(0) != '#' && x.length == 6 ?
-    setPrColor('#' + e.target.value)
-    :
-    console('no hex falue');
-  }
-
-  function setSec(e) {
-    x = e.target.value
-    x.charAt(0) == '#' && x.length == 7 ? 
-      setSecColor(x)
-    : 
-    x.charAt(0) != '#' && x.length == 6 ?
-    setSecColor('#' + e.target.value)
-    :
-    console('no hex falue');
-  }
-
-  function setText(e) {
-    x = e.target.value
-    x.charAt(0) == '#' && x.length == 7 ? 
-      setTextColor(x)
-    : 
-    x.charAt(0) != '#' && x.length == 6 ?
-    setTextColor('#' + e.target.value)
-    :
-    console('no hex falue');
-  }
-
   return (
     <div>
-      <nav>
+      <nav className='flex'>
         <div>
           Background Color
           <input type="color" value={bgColor} onChange={(e) => setBgColor(e.target.value)}/>
-          <input type="text" placeholder={bgColor} onChange={(e) => {setBg(e)}}/>
+          <input type="text" placeholder={bgColor} onChange={(e) => {setValue(e, setBgColor)}}/>
         </div>
         <div>
           Primary Color
           <input type="color" value={prColor} onChange={(e) => setPrColor(e.target.value)}/>
-          <input type="text" placeholder={prColor} onChange={(e) => {setPr(e)}}/>
+          <input type="text" placeholder={prColor} onChange={(e) => {setValue(e, setPrColor)}}/>
         </div>
         <div>
           Secondary Color
           <input type="color" value={secColor} onChange={(e) => setSecColor(e.target.value)}/>
-          <input type="text" placeholder={secColor} onChange={(e) => {setSec(e)}}/>
+          <input type="text" placeholder={secColor} onChange={(e) => {setValue(e, setSecColor)}}/>
         </div>
         <div>
           Text Color
           <input type="color" value={textColor} onChange={(e) => setTextColor(e.target.value)}/>
-          <input type="text" placeholder={textColor} onChange={(e) => {setText(e)}}/>
+          <input type="text" placeholder={textColor} onChange={(e) => {setValue(e, setTextColor)}}/>
         </div>
       </nav>
       <Page/>

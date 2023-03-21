38eee36a7ce455aa69d9ec248b451b7cd5430ed4
Jannis Milz <jannis.milz@econ.uzh.ch>
2023-03-21 11:12:54 +0100

    WIP UI

--- pricks/src/Page.jsx
+++ pricks/src/Page.jsx
@@ -1,17 +1,61 @@
 import { useParams } from 'react-router-dom'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar'
 
 export default function Page() {
   return (
     <div className='flex'>
       <Sidebar />
       <div className='text-textColor bg-bgColor w-full'>
-        <div className='bg-primary' >Primary Color</div>
-        <div className='bg-secondary' >Secondary Color</div>
-        <div className='bg-bgPrimary' >Background Primary</div>
-        <div className='bg-bgSecondary' >Background Secondary</div>
-        <div className='text-textPrimary' >Text Primary</div>
-        <div className='text-textSecondary' >Text Secondary</div>
+        {/* <div className='bg-primary' >Primary Color</div>
+          <div className='bg-secondary' >Secondary Color</div>
+          <div className='bg-bgPrimary' >Background Primary</div>
+          <div className='bg-bgSecondary' >Background Secondary</div>
+          <div className='text-textPrimary' >Text Primary</div>
+          <div className='text-textSecondary' >Text Secondary</div> */}
+        {/* Navbar */}
+        <nav className='h-20'>
+          <div className='h-1 bg-gradient-to-r from-primary to-secondary'></div>
+          <div className='flex items-center justify-between h-[4.75rem] px-12'>
+            <div>
+              Logo
+            </div>
+            <div className='flex gap-8'>
+              <a href="#home">Home</a>
+              <a href="#about">About</a>
+              <a href="#showcase">Showcase</a>
+            </div>
+          </div>
+        </nav>
+        {/* Hero */}
+        <section id="home">
+          <div className="mx-16 flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+                <br className="hidden lg:inline-block" />readymade gluten
+              </h1>
+              <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+              <div className="flex justify-center">
+                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+              </div>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+            </div>
+          </div>
+        </section>
+        {/* About */}
+        <section id="about">
+          About
+        </section>
+        {/* Showcase */}
+        <section id="showcase">
+          Showcase
+        </section>
+        {/* Footer */}
+        <footer>
+          Footer
+        </footer>
       </div>
     </div>
   )
